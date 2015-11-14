8d3dfe4d02c27ca537ba60b88bbdbdb277a5bbfc
Pim Schreurs <pimschreurs@gmail.com>
2015-11-14 19:21:10 +0100

    Visualize player direction in diagram too

--- js/src/diagramrenderer.js
+++ js/src/diagramrenderer.js
@@ -90,6 +90,18 @@ WormholeGeometry.prototype.getPoint = function(x, y) {
   }
 };
 
+WormholeGeometry.prototype.getXTangent = function(x, y) {
+  var distanceToThroat = Math.max(Math.abs(x) - this._space.throatLength, 0);
+
+  var derivX = new THREE.Vector3(
+    distanceToThroat * Math.cos(y) * (x < 0.0 ? -1.0 : 1.0),
+    distanceToThroat * Math.sin(y) * (x < 0.0 ? -1.0 : 1.0),
+    this._space.radius
+  );
+
+  return derivX.normalize();
+};
+
 WormholeGeometry.prototype.getNormal = function(x, y) {
   var distanceToThroat = Math.abs(x) - this._space.throatLength;
 
@@ -127,13 +139,26 @@ function DiagramRenderer(space, limit)
   this._camera = new THREE.PerspectiveCamera(60, 1, 1, 1000);
   this._camera.up.set(0, 0, 1);
 
+  this._limit = limit;
+
   this._geometry = new WormholeGeometry(space, -limit, limit);
+
+  var playerMaterial = new THREE.MeshBasicMaterial;
+
+  this._player = new THREE.Object3D;
 
   this._dotMesh = new THREE.Mesh(
     new THREE.SphereGeometry(0.1, 32, 32),
-    new THREE.MeshBasicMaterial
+    playerMaterial
   );
-  this._scene.add(this._dotMesh);
+  this._player.add(this._dotMesh);
+  this._directionMesh = new THREE.Mesh(
+    new THREE.BoxGeometry(0.1, 0.1, 0.4),
+    playerMaterial
+  );
+  this._directionMesh.position.z = -0.2;
+  this._player.add(this._directionMesh);
+  this._scene.add(this._player);
 
   var loader = new THREE.TextureLoader();
   var gridTexture = loader.load("textures/grid.png");
@@ -196,10 +221,35 @@ DiagramRenderer.prototype = {
 
     var radius = Math.sqrt(point.x * point.x + point.y * point.y);
 
-    this._camera.position.set(point.x * 8 / radius, point.y * 8 / radius, 0);
+    var scale = this._limit + 1;
+    this._camera.position.set(point.x / radius * scale, point.y / radius * scale, 0);
     this._camera.lookAt(point);
 
-    this._dotMesh.position.copy(point);
+    this._player.position.copy(point);
+
+    // Create and set a base rotation matrix
+    var normal = this._geometry.getNormal(object.position.x, object.position.z);
+    var tangent = this._geometry.getXTangent(object.position.x, object.position.z);
+    var bitangent = (new THREE.Vector3).crossVectors(normal, tangent);
+
+    var rotationMatrix = new THREE.Matrix4;
+    rotationMatrix.elements[0] = normal.x;
+    rotationMatrix.elements[1] = normal.y;
+    rotationMatrix.elements[2] = normal.z;
+
+    rotationMatrix.elements[4] = tangent.x;
+    rotationMatrix.elements[5] = tangent.y;
+    rotationMatrix.elements[6] = tangent.z;
+
+    rotationMatrix.elements[8] = bitangent.x;
+    rotationMatrix.elements[9] = bitangent.y;
+    rotationMatrix.elements[10] = bitangent.z;
+
+    this._player.quaternion.setFromRotationMatrix(rotationMatrix);
+
+    // Rotate player visualization accordingly
+    this._player.rotateY(object.rotation.x);
+    this._player.rotateX(object.rotation.y);
 
     renderer.render(this._scene, this._camera, null, false);
   }
