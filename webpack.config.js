90298b4e5734ad3be045eddd9a6abd603aa90032
Pim Schreurs <pimschreurs@gmail.com>
2015-11-14 16:23:06 +0100

    Make npm scripts easier to use

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,8 @@ var webpack = require("webpack");
 
 module.exports = {
   entry: "./js/src/main.js",
-  devtool: "source-map",
   output: {
     path: "./js",
     filename: "dist.js"
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
-  ]
 };
